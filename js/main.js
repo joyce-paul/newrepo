c74900c077b68a05eab8c37ee83158a2e1ee7ded
Joyce Paul <joyce_paul@hotmail.com>
2019-12-02 20:47:43 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
+// General Assembly's Memory Game
+
+// Card Variables
 var cards = [
 {
-rank: "queen",
-suit: "hearts",
-cardImage: "images/queen-of-hearts.png"
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
 },
 {
-rank: "queen",
-suit: "diamonds",
-cardImage: "images/queen-of-diamonds.png"
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
 },
 {
-rank: "king",
-suit: "hearts",
-cardImage: "images/king-of-hearts.png"
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
 },
 {
-rank: "king",
-suit: "diamonds",
-cardImage: "images/king-of-diamonds.png"
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
 }
 ];
 
@@ -25,24 +28,38 @@ var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 }
 
-function flipCard(cardId) {
+
+function flipCard() {
+	var cardId = this.getAttribute("data-id");
+	this.setAttribute("src", cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 }
 
-flipCard([0]);
 cardsInPlay.push(cards[0].rank);
 console.log(cards[0].cardImage); 
 console.log(cards[0].suit); 
 
-flipCard([2]);
 cardsInPlay.push(cards[2].rank);
 console.log(cards[2].cardImage); 
 console.log(cards[2].suit); 
 
+
 checkForMatch();
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
