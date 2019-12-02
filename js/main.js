9ba3bb66e6e2c4d4b6ad2f7cd8cbee4fbf54b5ea
Joyce Paul <joyce_paul@hotmail.com>
2019-12-02 10:59:08 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(["queen"]);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push("king");
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+}
+
+flipCard([0]);
+cardsInPlay.push(cards[0]);
+flipCard([2]);
+cardsInPlay.push(cards[2]);
+
+checkForMatch();
